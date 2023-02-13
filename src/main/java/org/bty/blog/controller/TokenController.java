5768e803505e072ee0ae9b384f063f7154e49660
鲍庭玉 <baotingyu1997@gmail.com>
2023-02-13 17:07:40 +0800

    improve refresh token

--- src/main/java/org/bty/blog/controller/TokenController.java
+++ src/main/java/org/bty/blog/controller/TokenController.java
@@ -8,7 +8,9 @@ import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
 import org.bty.blog.service.TokenService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.web.bind.annotation.*;
@@ -30,16 +32,23 @@ public class TokenController {
     private final TokenService tokenService;
     private final List<JWK> jwkList;
 
+    /**
+     * refresh token , 参照OAuth2规范 <br/>
+     * 地址：<a href="https://www.rfc-editor.org/rfc/rfc6749#page-47">rfc6749: Refreshing an Access Token</a>
+     * @param refreshToken
+     * @param grantType
+     * @return
+     */
     @PostMapping("/refresh")
-    public ResponseEntity<Map<String, Object>> refresh(@RequestBody String refreshToken) {
+    public ResponseEntity<Map<String, Object>> refresh(@RequestParam("refresh_token") String refreshToken, @RequestParam("grant_type")String grantType) {
 
-        if(Strings.isNullOrEmpty(refreshToken)){
-            return ResponseEntity.ok(Collections.singletonMap("msg","no refreshToken or invalid refreshToken"));
+        if (Strings.isNullOrEmpty(refreshToken) || Strings.isNullOrEmpty(grantType) || !grantType.equals(OAuth2ParameterNames.REFRESH_TOKEN)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("msg", "grantType not supported"));
         }
-        String accessToken = tokenService.refreshAccessToken(refreshToken);
-        return ResponseEntity.ok(Collections.singletonMap("accessToken",accessToken));
-    }
 
+        String accessToken = tokenService.refreshAccessToken(refreshToken);
+        return ResponseEntity.ok(Collections.singletonMap("accessToken", accessToken));
+    }
 
 
     /**
@@ -47,9 +56,9 @@ public class TokenController {
      * 链接必须基于TLS，本案例未配置
      */
     @GetMapping("/keys")
-    public ResponseEntity<Map<String, Object>> keys(){
+    public ResponseEntity<Map<String, Object>> keys() {
         List<Map<String, Object>> keys = jwkList.stream().map(JWK::toJSONObject).collect(Collectors.toList());
-        return  ResponseEntity.ok(Collections.singletonMap("keys",keys));
+        return ResponseEntity.ok(Collections.singletonMap("keys", keys));
     }
 
 }
